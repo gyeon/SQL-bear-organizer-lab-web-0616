2097f91ee235e370194962a840596ea77af82a09
gyeon <gyeon19@gmail.com>
2016-06-17 14:32:20 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO bears(name) VALUES
+('Mr. Chocolate');
+INSERT INTO bears(name) VALUES
+('Rowdy');
+INSERT INTO bears(name) VALUES
+('Tabita');
+INSERT INTO bears(name) VALUES
+('Sergeant Brown');
+INSERT INTO bears(name) VALUES
+('Melissa');
+INSERT INTO bears(name) VALUES
+('Grinch');
+INSERT INTO bears(name) VALUES
+('Wendy');
+INSERT INTO bears(name) VALUES
+(NULL);
+
+
+
+-- ('Mr. Chocolate', 2, 'M', 'brown', 'calm', 1);
+-- ('Rowdy', 5, 'M', 'brown', 'rowdy', 1);
+-- ('Tabita', 8, 'F', 'black', 'good', 1);
+-- ('Sergeant Brown', 13, 'M', 'black', 'playful', 1);
+-- ('Melissa', 19, 'F', 'brown', 'happy', 0);
+-- ('Grinch', 9, 'M', 'black', 'grouchy' 1);
+-- ('Wendy', 15, 'F', 'brown', 'lazy', 0);
+-- (NULL, 4, 'F', 'brown', 'playful', 1);
